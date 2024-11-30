52d2a30c1da54a0df8674ebf90f9be599fa6240f
Ivan Kabanov <ivakab22@gmail.com>
2024-11-30 21:27:16 +0100

    18

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -15,14 +15,14 @@ export const Form = () => {
       subject,
     }
     tg.sendData(JSON.stringify(data))
-  }, [country, street, subject])
+  }, [country, street, subject, tg])
 
   useEffect(() => {
     tg.onEvent('mainButtonClicked', onSendData)
     return () => {
       tg.offEvent('mainButtonClicked', onSendData)
     }
-  }, [onSendData])
+  }, [onSendData, tg])
 
   useEffect(() => {
     tg.MainButton.setParams({
