df756eb631861a619dc1887eca3a83617ddebe46
Ivan Kabanov <ivakab22@gmail.com>
2024-11-30 20:22:22 +0100

    12

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import styles from './Form.module.css'
+
+export const Form = () => {
+  return <div>Form</div>
+}
---
+++ src/components/ProductList/ProductList.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import styles from './ProductList.module.css'
+
+export const ProductList = () => {
+  return <div>ProductList</div>
+}
