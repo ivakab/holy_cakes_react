18748a43118e834cb0aaab88cf986ac165b6284d
Ivan Kabanov <ivakab22@gmail.com>
2025-01-15 22:46:27 +0100

    set up config

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,4 @@ export const pathResolver = (p: string) => resolve(__dirname, '.', p)
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    manifest: true,
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name].[hash][extname]',
-        chunkFileNames: 'assets/[name].[hash].ts',
-        entryFileNames: 'assets/[name].[hash].ts',
-      },
-    },
-  },
 })
